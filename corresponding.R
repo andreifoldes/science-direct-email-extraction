42820e41f717af9247a093174db7f5102a1fa0fd
andreifoldes <foldes.andrei@gmail.com>
2017-12-12 23:39:23 +0100

    Update corresponding.R

--- corresponding.R
+++ corresponding.R
@@ -3,17 +3,14 @@
 #rm(list = ls(all = TRUE))
 
 #install if not installed
-list.of.packages <- c("rscopus", "statcheck", "dplyr", "XML", "RPostgreSQL", "pdftools")
+list.of.packages <- c("rscopus", "statcheck", "dplyr", "XML", "RPostgreSQL", "pdftools", "stringr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 #load
 lapply(list.of.packages, require, character.only = TRUE)
 
-journalIndex <- 0
-dateVal  <- ""
-projectid<- 0
 
-id<-''
+id<-'d710a9ee6a1f68f3eca5b6f8d3533b0e'
 api_key = get_api_key(id, error = FALSE)
 options(elsevier_api_key_filename = id)
 options(elsevier_api_key = id)
@@ -22,7 +19,7 @@ options(warn = -1)
 
 #create list of ScienceDirect Psychology articles via CopyPaste on the web search interface
 #scopusJournalList20171005 <-read.csv("C:\\Users\\User\\Documents\\Projects\\correspondingAuthor\\ScienceDirectJournalList.csv", header = FALSE, sep= ";")
-scopusJournalList20171005 <-read.csv("", 
+scopusJournalList20171005 <-read.csv("ScienceDirectJournalList.csv", 
                                      header = FALSE, sep= ";", fileEncoding = "LATIN2")
 
 
@@ -31,94 +28,102 @@ pg = dbDriver("PostgreSQL")
 
 # Local Postgres.app database; no password by default
 # Of course, you fill in your own database information here.
-con = dbConnect(pg, user="", password="",
-                host="localhost", port=, dbname="")
-
-# check random table 
-dbExistsTable(con, "failedxml")
+con = dbConnect(pg, user="postgres", password="andreilikesoctopuses",
+                host="localhost", port=5432, dbname="miningdb")
 
 ###code
 
 ## import extraction method
-source("C:\\Users\\User\\Documents\\Projects\\correspondingAuthor\\extractMetadata.R")
-source("C:\\Users\\User\\Documents\\Projects\\correspondingAuthor\\resultValidator.R")
+source("extractMetadata.R")
 
+months <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+journalIndex <- 1
+dates  <- c("January 2010", "February 2010", "March 2010")
 
 #helperfuncion
-getResultSet <- function(journalIndex, dateVal, resultOffset=1)
+getResultSet <- function(journalIndex, dateIndex, resultOffset=0)
 {
   res = generic_elsevier_api(
     query = paste(
       "DOCTYPE ( ar )  AND",
       "SRCTITLE (",scopusJournalList20171005$V1[journalIndex],")",
-      "AND  PUBDATETXT(", dateVal,")"
+      "AND  PUBDATETXT(", dates[dateIndex],")"
     ),
     type = "search",
     search_type = "scopus",
     api_key = api_key,
     start = resultOffset,
-    count = 100
+    count = 10
   )
 }
 
-extractMeta = function(journalIndex, dateVal){
+extractMeta<-function(i,j=1){
   
-  while (journalIndex <= nrow(scopusJournalList20171005))
-  {
-    res <- getResultSet(journalIndex, dateVal)
-    
-    res <- resultValidator(res)
-    
-    #handle large resultSet condition
-    while (as.numeric(res$content$`search-results`$`opensearch:totalResults`) >= 5000)
+  while(j<=length(dates)){ 
+    while (i < nrow(scopusJournalList20171005))
     {
-      message(paste("[ResCount is above 5000] Current page index is:", journalIndex, ' ', 1))
+      res <- getResultSet(i,j)
       
-      dbWriteTable(con,'repeatquery',data.frame(journalIndex,dateVal,projectid), row.names=FALSE,append=TRUE)
-      
-      
-      journalIndex<-journalIndex+1
-      res <-getResultSet(journalIndex, dateVal)
-      res <-resultValidator(result)
-      if(journalIndex == nrow(scopusJournalList20171005))
+      #handle empty resultSet condition
+      while (is.null(res$content$`search-results`$`opensearch:totalResults`) 
+             || as.numeric(res$content$`search-results`$`opensearch:totalResults`) >= 5000
+             || as.numeric(res$content$`search-results`$`opensearch:totalResults`) == 0 
+             || res$get_statement$status_code!=200)
       {
-        return() 
+        
+        i<-i+1
+        res <-getResultSet(i,j)
+        if(i == nrow(scopusJournalList20171005))
+        {
+          return() 
+        }
+        
+        if(!is.null(res$content$`search-results`$`opensearch:totalResults`) ){
+          if(as.numeric(res$content$`search-results`$`opensearch:totalResults`) >= 5000){
+            dbWriteTable(con,'repeatquery',data.frame(cbind(i,j)), row.names=FALSE, append=TRUE)
+          }
+        }
+        
       }
-    }
-    
-    totalResCount <-if(is.null(res$content$`search-results`$`opensearch:totalResults`) 
-                       || res$content$`search-results`$`opensearch:totalResults` == 0 ) NULL else as.numeric(res$content$`search-results`$`opensearch:totalResults`)
-    
-    ### MAIN - examine all astracts in resultsSet in 100 length batches
-    if(!is.null(totalResCount))
-    {
-      for (k in seq(1, as.numeric(totalResCount), by = 100))
+      
+      
+      totalResCount <-
+        as.numeric(res$content$`search-results`$`opensearch:totalResults`)
+      
+      ### MAIN - examine all abstracts in resultsSet in 10 length batches
+      
+      for (k in seq(0, as.numeric(totalResCount), by = 10))
       {
-        if (k == 1)
+        if (k == 0)
         {
           res <- res #do nothing
         } else{
-          res <- getResultSet(journalIndex, dateVal, k)
+          res <- getResultSet(i, j, k)
         }
         
         
         idSet <- NULL
         
-        message(paste("Current page index is:", journalIndex," ", dateVal, " ", k))
+        message(paste("Current page index is:", "date",j, "journal", i, 'offset ', k))
         
         message(paste(res$get_statement$headers$`x-els-status`))
         
-        if(res$get_statement$headers$`x-els-status`!="OK") stop("request not ok")
+        if(res$get_statement$headers$`x-els-status`!="OK"){
+          dbWriteTable(con,'repeatquery',data.frame(cbind(i,j)), row.names=FALSE, append=TRUE)
+        }
         
         if(!is.null(res$content$`search-results`$entry))
         {
           entryCount <- length(res$content$`search-results`$entry)
           
+          metadataBatch <- NULL
+          
           #Total number of results examined in a given batch
           for (l in 1:entryCount){
             
             #scopusId
-            message("batchIndex:", journalIndex," ", dateVal, " ", k, " ", l)
+            message("batchIndex:","date ",j," ", i, " ", k, " ", l)
             if (is.null(res$content$`search-results`$entry[[l]]$`dc:identifier`))
             {
               idSet[l] <- NA
@@ -133,20 +138,24 @@ extractMeta = function(journalIndex, dateVal){
             {
               s <- NA
               
-            }else{
+            }
+            else{
               
               link<-paste("http://api.elsevier.com/content/article/scopus_id/",substr(idSet[l],11,30),"?apiKey=",id,"&httpAccept=text%2Fxml", sep = "")
               
-              resultList <-tryCatch({extractMetadata(link, con, idSet[l])}
-                                    ,error = function(err) { message(err)})
+              resultList<-NULL
+              
+              tryCatch({resultList <- extractMetadata(link, con, idSet[l])},error=function(e){resultList<-NULL})
               
               if(!is.null(resultList)){
+                
                 result <- tryCatch({
                   dbWriteTable(con,'articles',resultList[[1]], row.names=FALSE,append=TRUE)
                 },error = function(err) {
                   
                   # error handler picks up where error was generated
                   print(paste("MY_ERROR:  ","ID:",idSet[l], "index: ", l , err))
+                  
                   dbWriteTable(con,'duplicates',resultList[[1]], row.names=FALSE, append=TRUE)
                   
                 }) # END tryCatch
@@ -183,17 +192,9 @@ extractMeta = function(journalIndex, dateVal){
                 }
                 
               }
-              
-              
             }
             
-            #metadataBatch <- rbind(metadataBatch,s)
-            
-            
-            
           }
-          
-          #write.table(metadataBatch,paste(path,"\\",paste("metadata",i,k,dateIndex, sep = "_"), ".csv",sep=""), col.names=FALSE, row.names=FALSE, sep=";")
           
           
         }
@@ -201,17 +202,18 @@ extractMeta = function(journalIndex, dateVal){
         
         
       }
+      
+      i<- i+1
+      do.call(file.remove, list(list.files("pdfs", full.names = TRUE)))
+      
+      
     }
-
-    
-    journalIndex<- journalIndex+1
-    
-  }
-  
+    j<-j+1
+    i<-1
+    print("nextdate")
+  } 
 }
 
-
-
-#mustRepeat <- NULL
+extractMeta(i)
 
 
