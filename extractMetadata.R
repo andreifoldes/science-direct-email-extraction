19792c3bad6d89c43a8dc145885ee8d029a0caf2
andreifoldes <foldes.andrei@gmail.com>
2017-12-12 23:40:25 +0100

    Update extractMetadata.R

--- extractMetadata.R
+++ extractMetadata.R
@@ -41,8 +41,6 @@ extractMetadata <- function(link, con, ar_id){
   authors <- data.frame(do.call('rbind', strsplit(as.character(authors$names),',',fixed=TRUE)))
   authors <- cbind(authors,index=rownames(authors))
   
-
-  
   doi       <-  if(!is.null(core$doi)) core$doi else NA
   scopusId  <-  if(!is.null(xml_data$`scopus-id`)) xml_data$`scopus-id` else NA
   title     <-  if(!is.null(core$title)) core$title else NA
@@ -55,16 +53,16 @@ extractMetadata <- function(link, con, ar_id){
   coverdate <- if(!is.null(core$coverDate)) core$coverDate else NA
   publisher <- if(!is.null(core$publisher)) core$publisher else NA
   pubtype   <- if(!is.null(core$pubType)) core$pubType else NA
+  openaccess <-if(!is.null(core$openaccess)) core$openaccess else NA
   
   tmp<-core[which(names(xml_data$coredata)=="subject")]
   
-  if(length(tmp)!=0){
-    if(length(tmp)==1)
-    {
-      keywords<-data.frame(keywords = matrix(unlist(strsplit(tmp$subject, ",")), byrow=TRUE))
-    }else{
-      keywords <- data.frame(keywords = do.call("rbind", tmp))
-    }
+  if(length(tmp)==1){
+    keywords <- strsplit(as.character(tmp), ',')
+    keywords <- data.frame(keywords = do.call("cbind", keywords))
+    keywords <- cbind(keywords,index=rownames(keywords))
+  }else if(length(tmp)>1){
+    keywords <- data.frame(keywords = do.call("rbind", tmp))
     keywords <- cbind(keywords,index=rownames(keywords))
   }else{
     keywords        <-  NULL
@@ -72,8 +70,10 @@ extractMetadata <- function(link, con, ar_id){
   
   sourceid <- scopusId
   sourceidtype <- "scopusID"
-
-
+  projectid<- 21
+  
+  articleDF <- data.frame(doi, sourceid, sourceidtype, title, publicationname, type, volume, issueidentifier, pagerange
+                          ,number, coverdate, publisher, pubtype, projectid, openaccess)
   
   authorsDF <- data.frame(doi, sourceid, sourceidtype, givenname=authors$X2, surname = authors$X1, index=authors$index)
   
@@ -84,16 +84,14 @@ extractMetadata <- function(link, con, ar_id){
     keywordsDF <- NULL
   }
   
-  Sys.sleep(sample(1:5, 1))
-  
   link<-paste("http://api.elsevier.com/content/article/doi/",doi,"?apiKey=",api_key,"&httpAccept=application%2Fpdf", sep = "")
-  output<-paste("C:\\Users\\User\\Documents\\Projects\\correspondingAuthor\\tmp\\",gsub("/","_",doi),".pdf",sep="")
+  output<-paste("pdfs/",gsub("/","_",doi),".pdf",sep="")
   
   tryCatch({
-
-  download.file(link,output, mode="wb")
-  pagenum<-pdf_info(output)$pages
-  fulltextStatcheck <- checkPDF(output)
+    
+    download.file(link,output, mode="wb")
+    pagenum<-pdf_info(output)$pages
+    fulltextStatcheck <- checkPDF(output)
   }, error = function(e){
     dbWriteTable(con,'failedDownloads',data.frame(doi, link), row.names=FALSE,append=TRUE)
     fulltextStatcheck <- NULL
@@ -103,7 +101,7 @@ extractMetadata <- function(link, con, ar_id){
   
   if(!is.null(fulltextStatcheck))
   {
-    fulltextStatcheckDF<- data.frame(doi=doi, fulltextStatcheck, fromfulltext=1, pagenum)
+    fulltextStatcheckDF<- data.frame(doi, fulltextStatcheck, fromfulltext=1, pagenum)
     names(fulltextStatcheckDF) <- gsub("\\.","",tolower(names(fulltextStatcheckDF)))
     fulltextStatcheckDF$source<-NULL
     
@@ -115,9 +113,10 @@ extractMetadata <- function(link, con, ar_id){
   
   if(!length(abstractStatcheck)==0)
   {
-    abstractStatcheckDF<- data.frame(doi=doi, abstractStatcheck, fromfulltext=0)
+    abstractStatcheckDF<- data.frame(doi, abstractStatcheck, fromfulltext=0)
+    abstractStatcheckDF$Source<-NULL
+    
     names(abstractStatcheckDF) <- gsub("\\.","",tolower(names(abstractStatcheckDF)))
-    abstractStatcheckDF$source<-NULL
     
   }else{
     abstractStatcheckDF<-NULL
@@ -139,13 +138,43 @@ extractMetadata <- function(link, con, ar_id){
   if(is.null(cEmail)){
     #stop("email missing")
     emailDF<-NULL
+    
+    tmp<-"Laura.L.Steckley@strath.ac.uk.	 Laura	Steckley"
+    pat<-"[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}"
+    tmp[grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(tmp), ignore.case=TRUE)]
+    
+    tryCatch({
+      pdfText <- pdf_text(output)
+      cEmail  <- str_extract(pdfText, "\\S*@\\S*")
+      
+      #remove unnecessary surplus string, dots and commas if there are any
+      cEmail<-gsub("\\.$|\\,$", "", strsplit(cEmail, '\t')[[1]][1])
+      
+      index   <- min(which(str_detect(cEmail, regex(as.character(authors$X1), ignore_case = T))  == TRUE))
+      
+      #try to find name belonging to text mined email
+      if(is.finite(min(which(str_detect(cEmail, as.character(authors$X1)) == TRUE))))
+      {
+        emailDF<-data.frame(doi,email=cEmail,givenname=authors$X2[index], surname = authors$X1[index])
+      }else{
+        tmp <- strsplit(pdfText, "\n")
+        index <- min(unlist(lapply(tmp, function(ch) grep("E-mail", ch))))
+        tmp <- gsub(".*\\((.*)\\).*", "\\1", tmp[[1]][index])[1]
+        
+        if(grepl("\\d|:", tmp)){
+          emailDF<-data.frame(doi,email=cEmail, givenname=NA, surname = NA )
+        }else{
+          emailDF<-data.frame(doi,email=cEmail, givenname=NA, surname = tmp )
+        }
+        
+      }
+      
+    })
+    
+    
   }else{
     emailDF<-data.frame(doi,email=cEmail,givenname=cName$X2, surname = cName$X1)
   }
-  
-  
-  articleDF <- data.frame(doi, sourceid, sourceidtype, title, publicationname, type, volume, issueidentifier, pagerange
-                          ,number, coverdate, publisher, pubtype, projectid, pagenum)
   
   return(list(articleDF, authorsDF, keywordsDF, fulltextStatcheckDF, abstractStatcheckDF, emailDF))
   
